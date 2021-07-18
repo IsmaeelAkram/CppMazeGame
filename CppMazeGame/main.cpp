9ae1e731d63d9fd4b920693af2d4d13860f4acef
Ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2021-07-18 18:08:55 -0400

    Initial Commit

---
+++ CppMazeGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppMazeGame
+//
+//  Created by Ismaeel Akram on 7/18/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
